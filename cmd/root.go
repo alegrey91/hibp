4b408917d2683cce7a6a15ae1fb7e2608251aa1b
alessio <ale_grey_91@hotmail.it>
2021-01-31 19:06:29 +0100

    Added comments

--- cmd/root.go
+++ cmd/root.go
@@ -30,12 +30,8 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "hibp",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `HIBP stand for HaveIBeenPwned.
+It is a simple client to communicate with haveibeenpwned (haveibeenpwned.com) APIs.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
